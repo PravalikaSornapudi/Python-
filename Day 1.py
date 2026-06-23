2ae27dd38e97052a01ed4e5cf52dc872ea2bf8fd
Pravalika Sornapudi <pravalikasornapudi@gmail.com>
2026-06-23 17:59:31 +0530

    Day 1 Done

---
+++ Day 1.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+
+# Operators
+
+print(2+3) #addition
+print(3-1) #subtraction
+print(2*3) #multiplication
+print(3+2)
+print(3/2) #division
+print(3**2) #exponential
+print(3%2) #modulus
+print(3//2) #floor division
+
+# Data Types
+
+print(type(10))  #int
+print(type(3.14)) #float
+print(type(1+3j)) #complex
+print(type('Pravalika')) #string
+print(type([1,2,3])) #list
+print(type({'name':'Pravalika'})) #dict
+print(type({9.8, 3.14, 2.7})) #set
