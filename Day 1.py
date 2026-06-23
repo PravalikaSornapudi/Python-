298458f52a65a85e9305fb17d9150bb4f0fb0360
Pravalika Sornapudi <pravalikasornapudi@gmail.com>
2026-06-23 18:02:46 +0530

    Day 1 Done

--- Day 1.py
+++ Day 1.py
@@ -17,6 +17,8 @@ print(type(10))  #int
 print(type(3.14)) #float
 print(type(1+3j)) #complex
 print(type('Pravalika')) #string
-print(type([1,2,3])) #list
+print(type([1,2,3])) #list - mutable, duplicates allowed, ordered
 print(type({'name':'Pravalika'})) #dict
-print(type({9.8, 3.14, 2.7})) #set
+print(type({9.8, 3.14, 2.7})) #set - mutable, no duplicates, unordered
+
+#tuple - immutable, duplicates allowed, ordered
